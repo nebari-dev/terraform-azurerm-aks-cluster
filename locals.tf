f8af8f6ef0b15fce93a60bb71db958287a0609ce
Tyler Potts <49161327+tylerpotts@users.noreply.github.com>
2026-05-21 13:38:22 -0500

    style: tofu fmt locals.tf column alignment

--- locals.tf
+++ locals.tf
@@ -12,7 +12,7 @@ locals {
     for name, ng in var.node_groups : name if ng.mode == "System"
   ]
   system_pool_name = length(local.explicit_system_pools) > 0 ? local.explicit_system_pools[0] : keys(var.node_groups)[0]
-  system_pool = merge(var.node_groups[local.system_pool_name], { mode = "System" })
+  system_pool      = merge(var.node_groups[local.system_pool_name], { mode = "System" })
 
   # User pools = everything except the system pool.
   user_pools = {
@@ -22,9 +22,9 @@ locals {
 }
 
 locals {
-  resource_group_name     = var.create_resource_group ? azurerm_resource_group.this[0].name     : data.azurerm_resource_group.existing[0].name
+  resource_group_name     = var.create_resource_group ? azurerm_resource_group.this[0].name : data.azurerm_resource_group.existing[0].name
   resource_group_location = var.create_resource_group ? azurerm_resource_group.this[0].location : data.azurerm_resource_group.existing[0].location
 
   vnet_id        = var.create_vnet ? azurerm_virtual_network.this[0].id : var.existing_vnet_id
-  node_subnet_id = var.create_vnet ? azurerm_subnet.nodes[0].id         : var.existing_node_subnet_id
+  node_subnet_id = var.create_vnet ? azurerm_subnet.nodes[0].id : var.existing_node_subnet_id
 }
